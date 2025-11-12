30a52940df2e5ef9d13c8d0e3ac2044e1dd41316
Karan09542 <karan09542@gmail.com>
2025-11-12 14:57:39 +0530

    ram

--- src/components/SumCalculator.js
+++ src/components/SumCalculator.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { use, useEffect } from 'react'
 
 const SumCalculator = () => {
+const [value, setValue] = useState(0);
 const [sum, setSum] = useState(0);
+
+useEffect(()=>{
+    setSum(prev => prev + value)
+},[value])
   return (
     <div>
-        <input type="number" value={sum} onChange={(e) => setSum(prev => prev + Number(e.target.value))} />
+        <input type="number" value={value} onChange={(e) => setValue(prev => prev + Number(e.target.value))} />
         <p>Sum: {sum}</p>
     </div>
   )
